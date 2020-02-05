871d66ecbe86330efc6ae511a8653bf761d48804
BBoDDo <59723285+BBoDDo22@users.noreply.github.com>
2020-02-05 10:42:55 +0900

    Update medfilt.py

--- medfilt.py
+++ medfilt.py
@@ -23,22 +23,34 @@ def medfilt(A, n):
     [row, col] = A.shape 
     
     ar = np.zeros((n,n))
-    for r in range(row):
-        for c in range(col):
-            for j in range(-nn, nn + 1):
-                for k in range(-nn, nn + 1):
-                    if r+j <= -1 or r+j >= row or c+j <= -1 or c+j >= row or r+k <= -1 or r+k >= row or c+k <= -1 or c+k >= row:
-                        ar[j+1][k+1] = 0
-                    else:
-                        ar[j+1][k+1] = A[r+j][c+k]
-            
-            arr = np.sort(np.array(ar).ravel())
-            A_mf[r][c] = arr[len(arr)//2]
+    
+    if v == 0:
+        for r in range(row):
+            for c in range(col):
+                for j in range(-nn, nn + 1):
+                    for k in range(-nn, nn + 1):
+                        if r+j <= -1 or r+j >= row or c+j <= -1 or c+j >= row or r+k <= -1 or r+k >= row or c+k <= -1 or c+k >= row:
+                            ar[j+1][k+1] = 0
+                        else:
+                            ar[j+1][k+1] = A[r+j][c+k]
+
+                arr = np.sort(np.array(ar).ravel())
+                A_mf[r][c] = arr[len(arr)//2]
+                
+    elif v == 1:
+        for r in range(nn,row-nn):
+            for c in range(nn,col-nn):
+                # Extract the image nxn mask
+                ar = A[r-nn:r+nn+1, c-nn:c+nn+1]
+
+                # flattening the 2D array to 1D
+                arr = np.sort(np.array(ar).ravel())
+                A_mf[r][c] = arr[len(arr)//2] 
     
 #     A_cv2_mf = cv2.medianBlur(A, n)
     
-    plt.figure(), plt.imshow(A, cmap='gray')
-    plt.figure(), plt.imshow(A_mf, cmap='gray')
+#     plt.figure(), plt.imshow(A, cmap='gray')
+#     plt.figure(), plt.imshow(A_mf, cmap='gray')
 #     plt.figure(), plt.imshow(A_cv2_mf, cmap='gray')
     
     return A_mf
